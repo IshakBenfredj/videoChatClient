d48d926bab682b3bca50e51c5165a99895d46bf4
Ishak Benfredj <107993434+IshakBenfredj@users.noreply.github.com>
2023-12-30 16:57:08 +0100

    Add files via upload

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -9,27 +9,24 @@ export default function VideoPlayer() {
 
   return (
     <div className="w-full mt-5 bg-gray-700/80 rounded-3xl grid md:grid-cols-2 grid-cols-1 gap-4 overflow-hidden">
-      {stream ? (
-        <div className="relative">
-          <video
-            playsInline
-            muted
-            ref={myVideo}
-            autoPlay
-            className="w-full h-full object-contain"
-          />
-          {callAccepted && !callEnded && (
-            <button
-              onClick={leaveCall}
-              className="p-2 absolute font-bold bg-red-800 text-white w-full z-40 bottom-0"
-            >
-              close
-            </button>
-          )}
-        </div>
-      ) : (
-        <div className="p-4 text-white font-bold">No Video Camera</div>
-      )}
+      <div className="relative">
+        <video
+          playsInline
+          muted
+          ref={myVideo}
+          autoPlay
+          className="w-full h-full object-contain"
+        />
+        {callAccepted && !callEnded && (
+          <button
+            onClick={leaveCall}
+            className="p-2 absolute font-bold bg-red-800 text-white w-full z-40 bottom-0"
+          >
+            close
+          </button>
+        )}
+      </div>
+
       {callAccepted && !callEnded ? (
         <div>
           <video
